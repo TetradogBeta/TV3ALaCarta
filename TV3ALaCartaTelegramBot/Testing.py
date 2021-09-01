01edfc7e957f40f9b00d83e2d723b1298f89c037
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-09-01 19:49:00 +0200

    actualizando, falta la parte de las respuestas

--- TV3ALaCartaTelegramBot/Testing.py
+++
@@ -1,5 +0,0 @@
-from TV3ALaCarta import TV3ALaCarta
-from Element import Element
-
-for video in TV3ALaCarta.Cerca(TV3ALaCarta.GetCercaUrl("si no",2)):
-    print(Element.GetInfo(video.UrlJSon).Desc);
--- TV3ALaCartaTelegramBot/main.py
+++ TV3ALaCartaTelegramBot/main.py
@@ -1,20 +1,60 @@
-from TelegramBot import TelegramBot
+from TelegramHelper.Bot import Bot
+
+from TV3ALaCarta import TV3ALaCarta
+from Element import Element
 from os.path import exists
 import time
 import os
 import sys
 
-fileConfig="Config";
+def Main():
+    fileConfig="Config";
 
-if exists(fileConfig):
-    fConfig = open(fileConfig, "r");
-    config = fConfig.readlines();
-    fConfig.close();
-    token=config[0];
-elif len(sys.argv)>1:
-    token=sys.argv[1];
-    fConfig = open(fileConfig, 'w');
-    fConfig.writelines([token]);
-    fConfig.close();
-print("Iniciando TV3 A La Carta BOT v1.0")
-TelegramBot.Start(token).run_forever();
+    if exists(fileConfig):
+        fConfig = open(fileConfig, "r");
+        config = fConfig.readlines();
+        fConfig.close();
+        token=config[0];
+    elif len(sys.argv)>1:
+        token=sys.argv[1];
+        fConfig = open(fileConfig, 'w');
+        fConfig.writelines([token]);
+        fConfig.close();
+
+    bot=Bot(token,"TV3 A La Carta V2.0");
+    bot.Default.AddContains('ccma.cat',SendInfo);
+    bot.Default.AddContains("http",lambda cli:cli.SendText("Només links donats per la cerca de TV3 a La Carta!"));
+    bot.Default.Default=Cerca;
+    bot.AddCommand("Start", lambda cli,args:cli.SendText("1-Text a cercar\n2-URL a obtenir informació"));
+    bot.Start();
+
+def Cerca(cli):
+    if len(cli.Args)>2 and "pagina"== cli.Args[-2]:
+        pagina=int(cli.Args[-1]);
+        text=" ".join(cli.Args[:-2]);
+    else:
+        pagina=1;
+        text=" ".join(cli.Args);
+    total=0;        
+    for video in TV3ALaCarta.Cerca(TV3ALaCarta.GetCercaUrl(text,pagina)):
+        total+=1;
+        cli.SendPhoto(video.Img,video.ToMessage());
+    if total>0:
+        siguientePagina=pagina+1;    
+        cli.SendText(text+" Pagina "+str(pagina));
+    else:
+        cli.SendText("No s'ha trobat res!");
+
+def SendInfo(cli):
+    text=" ".join(cli.Args);
+    if "\n" in text:
+        url=text.split("\n")[-1];
+    elif text.startswith("http"):
+        if " " in text:
+            url=text.split(" ")[0];
+        else:
+            url=text;    
+    result=Element.GetInfo(url);
+    cli.SendPhoto(result.Img,result.Desc);
+
+Main();
